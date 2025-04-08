e819b7ec9e7a8ac30ed14ffc464e7a1a9ce135b1
Meir Garfinkel <meirgarfinkel@gmail.com>
2025-04-08 13:32:12 +0000

    feat: add health route

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
+    path("health/", lambda request: HttpResponse("OK")),
     path("admin/", admin.site.urls),
     path("gratify/", include("gratify.urls")),
     path("users/", include("users.urls")),
